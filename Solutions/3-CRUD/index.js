93cfb9c99a4f343b94108a7061896c775aa24688
kbrdn1 <kylianb1@icloud.com>
2023-11-03 14:56:58 +0100

    wip

--- Solutions/3-CRUD/index.js
+++ Solutions/3-CRUD/index.js
@@ -6,24 +6,35 @@ const fastify = Fastify({ // Création de l'instance Fastify
 
 // Connexion à la base de données
 fastify.register(mysql, {
-  connectionString: 'mysql://fastify:fastify@localhost:3306/fastify'
+  connectionString: 'mysql://fastify:fastify@localhost:3306/fastify' // mysql://user:password@host:port/database
 })
 
 
 
 // Declaration de la route GET /articles
 fastify.get('/articles', async (req, res) => {
-  const articles = await fastify.mysql.query('SELECT * FROM articles')
-
-  return res.send(articles).code(200)
+  await fastify.mysql.query('SELECT * FROM articles', [], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return res.send(result).code(200);
+  })
 })
   
   // Declaration de la route GET /articles/:id
 fastify.get('/articles/:id', async (req, res) => {
-  const { id } = req.params,
-    article = await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id])
-  
-  return article ? res.send(article).code(200) : res.code(404)
+  const { id } = req.params;
+  await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    if (result.length === 0) {
+      return res.code(404);
+    }
+    return res.send(result[0]).code(200);
+  })
 })
 
 // Declaration de la route POST /articles
@@ -33,12 +44,28 @@ fastify.post('/articles', async (req, res) => {
   if (!title || !content) // Vérification des données, si elles sont manquantes, on renvoie un code 400
     return res.code(400)
   
-  if (await fastify.mysql.query('SELECT * FROM articles WHERE title = ?', [title])) // Vérification que l'article n'existe pas déjà, si c'est le cas, on renvoie un code 409
+  if (await fastify.mysql.query('SELECT * FROM articles WHERE title = ?', [title], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return result.length > 0;
+  })) // Vérification que l'article n'existe pas déjà, si c'est le cas, on renvoie un code 409
     return res.code(409)
 
-  const result = await fastify.mysql.query('INSERT INTO articles (title, content, banner) VALUES (?, ?)', [title, content, banner])
-
-  return res.send({ id: result.insertId }).code(201)
+  await fastify.mysql.query('INSERT INTO articles (title, content, banner) VALUES (?, ?)', [title, content, banner], async (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [result.insertId], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.code(500);
+      }
+      return res.send(result[0]).code(201);
+    })
+  })
 })
 
 // Declaration de la route PUT /articles/:id
@@ -49,30 +76,61 @@ fastify.put('/articles/:id', async (req, res) => {
   if (!title || !content) // Vérification des données, si elles sont manquantes, on renvoie un code 400
     return res.code(400)
   
-  if (await fastify.mysql.query('SELECT * FROM articles WHERE title = ? AND id != ?', [title, id])) // Vérification que l'article n'existe pas déjà, si c'est le cas, on renvoie un code 409
+  if (await fastify.mysql.query('SELECT * FROM articles WHERE title = ? AND id != ?', [title, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return result.length > 0;
+  })) // Vérification que l'article n'existe pas déjà, si c'est le cas, on renvoie un code 409
     return res.code(409)
   
-  if (!await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id])) // Vérification que l'article existe, si ce n'est pas le cas, on renvoie un code 404
+  if (!await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return result.length > 0;
+  })) // Vérification que l'article existe, si ce n'est pas le cas, on renvoie un code 404
     return res.code(404)
 
 
-  const date = new Date(Date.now()).toISOString().slice(0, 19).replace('T', ' '), // Formatage de la date pour MySQL
-    { info } = await fastify.mysql.query('UPDATE articles SET title = ?, content = ?, banner = ?, updated_at = ? WHERE id = ?', [title, content, banner, date, id]),
-    article = await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id])
-
-  return res.send(article).code(200)
+  const date = new Date(Date.now()).toISOString().slice(0, 19).replace('T', ' ') // Formatage de la date pour MySQL
+  await fastify.mysql.query('UPDATE articles SET title = ?, content = ?, banner = ?, updated_at = ? WHERE id = ?', [title, content, banner, date, id], async (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.code(500);
+      }
+      return res.send(result[0]).code(200);
+    })
+  })
 })
 
 // Declaration de la route DELETE /articles/:id
 fastify.delete('/articles/:id', async (req, res) => {
   const { id } = req.params
 
-  if (!await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id])) // Vérification que l'article existe, si ce n'est pas le cas, on renvoie un code 404
+  if (!await fastify.mysql.query('SELECT * FROM articles WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return result.length > 0;
+  })) // Vérification que l'article existe, si ce n'est pas le cas, on renvoie un code 404
     return res.code(404)
 
-  await fastify.mysql.query('DELETE FROM articles WHERE id = ?', [id])
-
-  return res.code(204)
+  await fastify.mysql.query('DELETE FROM articles WHERE id = ?', [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.code(500);
+    }
+    return res.code(204);
+  })
 })
 
 // Lancement du serveur
